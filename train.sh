23bae1293e8e5cbbbb225f91122342b1cf2e4669
Csaba Palfi <cxp8380@yum.com>
2024-11-18 22:45:33 +0000

    2 gpus

--- train.sh
+++ train.sh
@@ -6,5 +6,5 @@ checkpoint_path=$1
 max_epochs=$2
 
 nohup bash -c \
-    "(torchrun --standalone --nproc_per_node=1 train_gpt2.py $checkpoint_path $max_epochs); \
+    "(torchrun --standalone --nproc_per_node=2 train_gpt2.py $checkpoint_path $max_epochs); \
     runpodctl remove pod $RUNPOD_POD_ID" > "$log_file" 2>&1 & disown
